8a6a6cb0453b79057f4d6cd17b724398822e7bef
nguyenanhtu123 <lovebooks29062k2@gmail.com>
2023-11-09 17:12:59 +0700

    refactor:SM-87-API-Create-Address

--- shipment-client/src/main/java/constants/Constants.java
+++ shipment-client/src/main/java/constants/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
   public static class Validate{
     public static final String NAME_BLANK= "name cannot be empty";
     public static final String DESCRIPTION_BLANK= "description cannot be empty";
-    public static final String PROVINCE_BLANK= "Province cannot be empty";
-    public static final String DISTRICT_BLANK= "Province cannot be empty";
-    public static final String WARD_BLANK= "Province cannot be empty";
-    public static final String DETAIL_BLANK= "Province cannot be empty";
+    public static final String PROVINCE_BLANK= "com.ncsgroup.shipment.client.dto.address.AddressRequest.provinceCode.NotNull";
+    public static final String DISTRICT_BLANK= "com.ncsgroup.shipment.client.dto.address.AddressRequest.districtCode.NotNull";
+    public static final String WARD_BLANK= "com.ncsgroup.shipment.client.dto.address.AddressRequest.wardCode.NotNull";
+    public static final String DETAIL_BLANK= "com.ncsgroup.shipment.client.dto.address.AddressRequest.detail.NotNull";
 
 
   }
--- shipment-server/src/main/java/com/ncsgroup/shipment/server/repository/address/AddressRepository.java
+++ shipment-server/src/main/java/com/ncsgroup/shipment/server/repository/address/AddressRepository.java
@@ -10,21 +10,32 @@ import org.springframework.data.repository.query.Param;
 
 public interface AddressRepository extends BaseRepository<Address> {
   @Query("""
-        SELECT new com.ncsgroup.shipment.server.dto.address.AddressResponse(
-                a.wardCode, a.districtCode, a.provinceCode, a.detail)
-                FROM Address a
-                WHERE (:keyword is null or
-                (lower(a.wardCode) LIKE lower(concat('%', :keyword, '%')) OR
-                lower(a.districtCode) LIKE lower(concat('%', :keyword, '%')) OR
-                lower(a.provinceCode) LIKE lower(concat('%', :keyword, '%')) OR
-                lower(a.detail) LIKE lower(concat('%', :keyword, '%'))) )
-                AND a.isDeleted = false
-                ORDER BY a.provinceCode
+            SELECT new com.ncsgroup.shipment.server.dto.address.AddressResponse
+            (a.id, w.nameEn,d.nameEn,p.nameEn, a.detail)
+            FROM Address a
+            LEFT JOIN Province p ON a.provinceCode = p.code
+            LEFT JOIN District d ON a.districtCode = d.code
+            LEFT JOIN Ward w ON a.wardCode = w.code
+            WHERE (:keyword is null or
+            (lower(p.nameEn) LIKE lower(concat('%', :keyword, '%')) OR
+            lower(d.nameEn) LIKE lower(concat('%', :keyword, '%')) OR
+            lower(w.nameEn) LIKE lower(concat('%', :keyword, '%')) OR
+            lower(a.detail) LIKE lower(concat('%', :keyword, '%'))) )
+            AND a.isDeleted = false
+            ORDER BY a.provinceCode
         """)
   Page<AddressResponse> search(Pageable pageable, String keyword);
 
-  @Query("select a from Address a where a.isDeleted=false ")
-  Page<AddressResponse> findAllAddress(Pageable pageable);
+@Query("""
+            SELECT new com.ncsgroup.shipment.server.dto.address.AddressResponse
+            (a.id, w.nameEn,d.nameEn,p.nameEn, a.detail)
+            FROM Address a
+            LEFT JOIN Province p ON a.provinceCode = p.code
+            LEFT JOIN District d ON a.districtCode = d.code
+            LEFT JOIN Ward w ON a.wardCode = w.code
+            WHERE a.isDeleted=false
+        """)
+Page<AddressResponse> findAllAddress(Pageable pageable);
 
   @Query("SELECT EXISTS(SELECT true FROM Address a WHERE a.id = :id AND a.isDeleted = false)")
   boolean existsById(@Param("id") String id);
@@ -36,7 +47,7 @@ public interface AddressRepository extends BaseRepository<Address> {
             LEFT JOIN Province p ON a.provinceCode = p.code
             LEFT JOIN District d ON a.districtCode = d.code
             LEFT JOIN Ward w ON a.wardCode = w.code
-            WHERE a.id = :id
+            WHERE a.id = :id AND a.isDeleted=false
         """)
   AddressResponse findAddressById(@Param("id") String id);
 }
